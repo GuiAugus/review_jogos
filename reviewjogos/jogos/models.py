7ff44fa8b25f713992e84745223dc38ced262fd6
Guilherme <augusto.g@outlook.com.br>
2023-01-11 21:02:27 -0300

    criacao models jogos

--- reviewjogos/jogos/models.py
+++ reviewjogos/jogos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=200, help_text="Digite um gênero de jogo")
+
+    def __str__(self):
+        return self.nome
+
+class Jogo(models.Model):
+    titulo = models.CharField(max_length=200)
+    desenvolvedora =  models.ForeignKey('Desenvolvedora', on_delete=models.SET_NULL, null=True)
+    sinopse = models.TextField(max_length=1000, help_text="Digite a sinopse do jogo.")
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('jogo-detalhe', args=[str(self.id)])
+
+class Desenvolvedora(models.Model):
+    nome = models.CharField(max_length=100)
+
+    class Meta: 
+        ordering = ['nome']
+
+    def get_absolute_url(self):
+        return reverse('desenvolvedora-detalhe', args = [str(self.id)])
+
+    def __str__(self):
+        return f'{self.nome}'
+
