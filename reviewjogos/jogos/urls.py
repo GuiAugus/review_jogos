92fc69747ca1f8cdf7a3a20b9aa15377690ac50c
Guilherme <augusto.g@outlook.com.br>
2023-01-12 21:09:23 -0300

    Pagina inicial

--- reviewjogos/jogos/urls.py
+++ reviewjogos/jogos/urls.py
@@ -1,9 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
-from django.views.generic import RedirectView
 
-urlpatterns += [
-    path('', RedirectView.as_view(url='/jogos/')),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-]
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+]  
--- reviewjogos/jogos/views.py
+++ reviewjogos/jogos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from jogos.models import Desenvolvedora, Jogo
 
-# Create your views here.
+
+def index(request):
+    num_jogos = Jogo.objects.all().count()
+    num_desenvolvedora = Desenvolvedora.objects.count()
+
+    context = {
+        'num_jogos' : num_jogos,
+        'num_desenvolvedora' : num_desenvolvedora,
+    }
+
+    return render(request, 'index.html', context=context)
